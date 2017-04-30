8f25ff9a403c444dd2fff58dc2ddb8d32cdde6f6
Alayna Buysse <alaynagrace@comcast.net>
2017-04-30 15:22:02 -0500

    added css

--- public/client.js
+++ public/client.js
@@ -4,7 +4,8 @@ function onReady(){
   //event listeners
   $('#add-task').on('click', addTask);
   $('.container').on('click', '#complete', completeFunc);
-  $('.container').on('click', '#delete', deleteFunc);
+  $(document).on('click', '#delete', deleteFunc);
+  $(document).on('click', '#incomplete', reAddFunc);
 
   getTasks();
 
@@ -21,10 +22,11 @@ function getTasks(){
       $('.finished').empty();
 
       for(var i = 0; i< res.length; i++){
-        var compButton = '<button id="complete" data-taskid="'+res[i].id+'">Complete</button>';
-        var deleteButton = '<button id="delete" data-taskid="'+res[i].id+'">Delete</button>';
+        var compButton = '<button id="complete" data-taskid="'+res[i].id+'"><img src="http://www.clker.com/cliparts/2/k/n/l/C/Q/transparent-green-checkmark-md.png"></button>';
+        var uncomp = '<button id="incomplete" data-taskid="'+res[i].id+'"><img src="http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons-256/blue-jelly-icons-arrows/007361-blue-jelly-icon-arrows-arrow-undo.png"></button>';
+        var deleteButton = '<button id="delete" data-taskid="'+res[i].id+'"><img src="https://lh3.googleusercontent.com/G2jzG8a6-GAA4yhxx3XMJfPXsm6_pluyeEWKr9I5swUGF62d2xo_Qg3Kdnu00HAmDQ=w300"></button>';
         if(res[i].completed){
-          $('.finished').append('<p class="completed">' + res[i].task + deleteButton + '<img src="https://d30y9cdsu7xlg0.cloudfront.net/png/835-200.png"></p>');
+          $('.finished').append('<p class="completed">'+ res[i].task + deleteButton + uncomp +'</p>');
         }else{
           $('.container').append('<p class="created-task">' + res[i].task + compButton + deleteButton + '</p>');
         }
@@ -39,23 +41,30 @@ function getTasks(){
 
 //add a task function
 function addTask(){
-  //take in values from input
-  var objectToSend={
-    task: $('#new-task').val(),
-    completed: false
-  };
 
-  //send that task to the server using ajax
-  $.ajax({
-    url: '/newTask',
-    type: 'POST',
-    data: objectToSend,
-    success: function(res){
-      console.log(res);
-      getTasks();
-      $('#new-task').val('');
-    }
-  });
+  if($('#new-task').val() === ''){
+    alert('Please enter a task');
+  }
+  else{
+    //take in values from input
+    var objectToSend={
+      task: $('#new-task').val(),
+      completed: false
+    };
+
+    //send that task to the server using ajax
+    $.ajax({
+      url: '/newTask',
+      type: 'POST',
+      data: objectToSend,
+      success: function(res){
+        console.log(res);
+        getTasks();
+        $('#new-task').val('');
+      }
+    });
+  }
+
 }
 
 // function that happens when a task is completed
@@ -67,19 +76,9 @@ function completeFunc(){
     data: {completed: true, id:$(this).data('taskid')},
     success: function(res){
       console.log(res);
-      // $(this).parent().removeClass('created-task').addClass('completed');
-      // $(this).parent().append('<img src="https://d30y9cdsu7xlg0.cloudfront.net/png/835-200.png">');
-      // $('.finished').append($(this).parent());
-      // $(this).hide();
-      // if($('.container').text() === ''){
-      //   $('.container').append('<p id="none">No current tasks</p>');
-      // }
       getTasks();
     }
   });
-
-
-
 }
 
 //function that happens when user wants to delete a task
@@ -100,4 +99,16 @@ function deleteFunc(){
       }
     });
   }
+}
+
+function reAddFunc(){
+  $.ajax({
+    url: '/completed',
+    type: 'POST',
+    data: {completed: false, id: $(this).data('taskid')},
+    success: function(res){
+      console.log(res);
+      getTasks();
+    }
+  });
 }
